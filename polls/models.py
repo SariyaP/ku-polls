8e3a7f598ac7c40c5e55b53e7e3ca9fb7e0b545c
Sariya Punprang <sariya.pu@ku.th>
2024-09-01 18:28:08 +0700

    Adding test case for is_publish and can vote. Also add end date to the database and migrated

--- polls/models.py
+++ polls/models.py
@@ -5,7 +5,8 @@ from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField(timezone.now())
+    end_date = models.DateTimeField('date end', null=True, blank=True)
 
     def __str__(self):
         return self.question_text
@@ -13,6 +14,20 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    def is_published(self):
+        return timezone.now() >= self.pub_date
+
+    def can_vote(self):
+        if self.end_date is not None:
+            if self.pub_date <=  timezone.now() <= self.end_date :
+                return True
+            if self.end_date < self.pub_date:
+                return False
+            return False
+        if self.end_date is None:
+            return self.pub_date <= timezone.now()
+        return True
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
