cf0b91022f9c6657aadb3600ae33dbc988c6d533
Sariya Punprang <sariya.pu@ku.th>
2024-09-14 17:45:10 +0700

    arrage question

--- polls/views.py
+++ polls/views.py
@@ -17,6 +17,7 @@ logger = logging.getLogger("polls")
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
+    sort = Question.objects.order_by('-pub_date')
 
     def get_queryset(self):
         """Return the last five published questions."""
